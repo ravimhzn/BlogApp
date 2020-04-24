59bf43edcd1a6775ef948829cc80aede7d67550f
Ravi Maharjan <ravi.maharjan2018@gmail.com>
2020-04-24 10:54:53 +1000

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BlogApp
-MVI
+A basic demo application that uses MVI architectural pattern and follows best practices as suggested by google.
+
+## Project specifications
+
+Project features:
+
+* 100% [Kotlin](https://kotlinlang.org/)
+* [Android Jetpack](https://developer.android.com/jetpack)
+* Clean architecture
+* Coroutines
+* JUnit4 and Mockito Test
+
+## Project Used:
+* Android Studio 3.6.1
+* Kotlin Version 1.3.71
+* Gradle Build Version 3.6.1
+* Android Version 10
+* JVM 1.8
---
+++ app/src/test/java/com/ravimhzn/blogapplication/ui/main/viewmodels/MainViewModelTest.kt
@@ -0,0 +1,43 @@
+package com.ravimhzn.blogapplication.ui.main.viewmodels
+
+import org.junit.Assert.*
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+
+class MainViewModelTest {
+
+    //Begin region for constants
+
+    //End region for constants
+
+    //Begin region for helper fields
+
+    //End region for helper fields
+
+
+    private var SUT: MainViewModel? = null
+
+    @Before
+    fun init() {
+        MockitoAnnotations.initMocks(this)
+        SUT = MainViewModel()
+
+    }
+
+    //Begin region for helper methods
+
+    //End region for helper methods
+
+    //Begin region for helper classes
+
+    //End region for helper classes
+}
